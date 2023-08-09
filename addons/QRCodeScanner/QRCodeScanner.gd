e15c21a9570440709ecdda879e9bdb93c16f0c1d
Syvies <ugo.saintandre@gmail.com>
2023-08-09 18:18:28 +0200

    Initial commit

---
+++ addons/QRCodeScanner/QRCodeScanner.gd
@@ -0,0 +1,16 @@
+@tool
+extends EditorPlugin
+
+const NODE_NAME: String = "QRCodeScanner"
+const NODE_INHERITANCE: String = "Node"
+const NODE_SCRIPT: Script = preload("res://addons/QRCodeScanner/scanner.gd")
+const NODE_ICON: Texture2D = preload("res://addons/QRCodeScanner/icon.png")
+
+func _enter_tree() -> void:
+	# Initialization of the plugin goes here.
+	add_custom_type(NODE_NAME, NODE_INHERITANCE, NODE_SCRIPT, NODE_ICON)
+
+
+func _exit_tree() -> void:
+	# Clean-up of the plugin goes here.
+	remove_custom_type(NODE_NAME)
